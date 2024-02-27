21fe34188ddfce9bedfcac8cce57b283ad50efe4
akauzinha47 <kau.vmenezes@gmail.com>
2024-02-27 17:09:14 -0300

    progresso

--- Tabela/script.js
+++ Tabela/script.js
@@ -1,15 +1,50 @@
 function adicionarItem(){
     //Obter valores dos campos digitados
     var nome = document.getElementById("nome").value;
-    var valor = document.getElementById("valor").value;
+    
     var quantidade = document.getElementById("quantidade").value;
-    var total = quantidade*valor.value;
+    var valorDoProduto = 0
 
-    // console.log(nome, valor, quantidade)
-    // console.log(nome, valor, quantidade)
+switch (nome) {
+
+    case "Pão":
+        valorDoProduto = 2,00
+       
+        break;
+    case "Carne":
+        valorDoProduto = 20,00
+        
+        break;
+
+    case "Queijo":
+        valorDoProduto = 1,00
+            
+        break;
+    case "Alface":
+        valorDoProduto = 1,00
+    
+        break;
+
+    case "Tomate":
+    valorDoProduto = 1,00
+    
+    break;
+
+    case "Bacon":
+        valorDoProduto = 4,00
+        
+        break;
+
+    case "Refri":
+    valorDoProduto = 5,00
+    
+    break;
+
+}
+var total = quantidade * valorDoProduto;
 
     //Validar de todas existem
-    if(!nome || !valor || !quantidade){
+    if(!nome || !quantidade){
         alert("Favor preencher os campos");
         return;
     }
@@ -26,9 +61,9 @@ function adicionarItem(){
 
     // Colocar valores nas celulas
     celulaDescrição.innerHTML = nome;
-    celulaValor.innerHTML = "R$ " + valor;
+    celulaValor.innerHTML = "R$ " + valorDoProduto;
     celulaQuantidade.innerHTML = quantidade;
-    celulaTotal.innerHTML = valor*quantidade;
+    celulaTotal.innerHTML = "R$ " + total;
 }
 
 function exportarParaExcel(){
